e15611fad68398c0542ce7a26a9e5573d0761a41
m7madmagdy <mohamedmagdysaber@gmail.com>
2022-03-18 22:51:38 +0200

    update fragment

--- app/src/main/java/com/kotlinapps/flipper/FlipperFragment.java
+++ app/src/main/java/com/kotlinapps/flipper/FlipperFragment.java
@@ -6,21 +6,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import com.kotlinapps.flipper.databinding.FragmentFlipperBinding;
 
-import com.kotlinapps.flipper.databinding.FragmentBlankBinding;
-
-public class BlankFragment extends Fragment {
-    FragmentBlankBinding binding;
+public class FlipperFragment extends Fragment {
+    FragmentFlipperBinding binding;
 
     int[] gallery_grid_Images = {R.drawable.fun, R.drawable.lawyer, R.drawable.women, R.drawable.birds,
             R.drawable.teachers, R.drawable.tools, R.drawable.x_rays, R.drawable.vet_doc};
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentBlankBinding.inflate(getLayoutInflater());
+        binding = FragmentFlipperBinding.inflate(getLayoutInflater());
 
         for (int gallery_grid_image : gallery_grid_Images) {
             setFlipperImage(gallery_grid_image);
